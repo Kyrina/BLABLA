d8268685dad5bf2bd9229372f7257d7836a57d7e
Kyrina <44334958+Kyrina@users.noreply.github.com>
2018-11-20 12:45:14 +0200

    Update lab4.php

--- lab4.php
+++ lab4.php
@@ -4,7 +4,7 @@ require_once('functions.php');
 echo "<br>"."&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"."Zavdanya2"."<br>";
 $array1= array(240,222,555,5464,756,854,945);
 $array2= array(945,556,1544,452,755,645,1054);
-zavdanya2($array1,$array2);
+
 echo "<br>"."&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp"."Zavdanya3"."<br>";
 $array = [11, -32, 445, -7, 845, 90, -98, 665, 5641, -1545,666];
 zavdanya3($array);
